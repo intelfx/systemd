b1e34aa36d7bcaf71c8112db3ca5cf181413d8f8
Ivan Shapovalov <intelfx@intelfx.name>
2024-02-22 19:34:23 +0100

    hostname-util: treat hostname == FALLBACK_HOSTNAME same as unset
    If kernel hostname is equal to FALLBACK_HOSTNAME (the compiled-in
    default), treat it the same as if it was empty or equal to "(none)".
    
    This way, a distribution may configure a default hostname both in the
    kernel and the systemd it ships, while at the same time allowing it to
    be overridden via os-release if specific builds (e. g. live CD) call for
    a different default.

--- src/shared/hostname-setup.c
+++ src/shared/hostname-setup.c
@@ -320,7 +320,7 @@ int gethostname_full(GetHostnameFlags flags, char **ret) {
                 return -errno;
 
         s = u.nodename;
-        if (isempty(s) || streq(s, "(none)") ||
+        if (isempty(s) || streq(s, "(none)") || streq(s, FALLBACK_HOSTNAME) ||
             (!FLAGS_SET(flags, GET_HOSTNAME_ALLOW_LOCALHOST) && is_localhost(s)) ||
             (FLAGS_SET(flags, GET_HOSTNAME_SHORT) && s[0] == '.')) {
                 if (!FLAGS_SET(flags, GET_HOSTNAME_FALLBACK_DEFAULT))
