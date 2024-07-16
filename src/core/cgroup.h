e30b4706c03e0c595aadb6441b0abeee1c50b7d8
Ivan Shapovalov <intelfx@intelfx.name>
2024-07-16 13:02:37 +0200

    core/cgroup: add "unset" and "default" values for ZSwapWriteback
    Instead of treating ZSwapWriteback (memory.zswap.writeback) as a bool
    and eagerly initializing it for each cgroup, let kernel inherit the
    values and introduce a dedicated "unset" state where we let the kernel
    do its thing.
    
    Additionally, introduce a fourth "default" state which corresponds to
    the `-1` value of the memory.zswap.writeback attribute, meaning "use
    global default" (this is not yet in the kernel; will be submitted
    shortly).

--- src/core/cgroup.h
+++ src/core/cgroup.h
@@ -196,7 +196,7 @@ struct CGroupContext {
         bool startup_memory_swap_max_set:1;
         bool startup_memory_zswap_max_set:1;
 
-        bool memory_zswap_writeback;
+        CGroupZSwapWriteback memory_zswap_writeback;
 
         Set *ip_address_allow;
         Set *ip_address_deny;
